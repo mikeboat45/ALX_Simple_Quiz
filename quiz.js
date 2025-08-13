d5bf06172d527c66e05e6bbe5fdafc436e4cb110
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-08-13 13:23:27 +0000

    Define checkAnswer function

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function checkAnswer() {
+    let correctAnswer = 4;
+
+    const userInput = document.querySelector("[name='quiz']");
+    const userAnswer = userInput.value;
+
+    feedbackInput = document.getElementById("feedback");
+    feedback = feedbackInput.value;
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done."
+        
+    } else {
+        feedback.textContent = "That's incorrect. Try again!"
+        return;
+    }
+}
+
+
+
